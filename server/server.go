21221ae7ba59366191a219434ce87b6cf484e0fb
Hwajin Kang <hwajinkang@Hwajinui-MacBookPro.local>
2021-12-13 04:57:22 +0000

    step1_passing

--- server/server.go
+++ server/server.go
@@ -5,38 +5,29 @@ import (
 	"math/rand"
 	"net"
 	"net/rpc"
-	"sync"
 	"time"
 	"uk.ac.bris.cs/gameoflife/stubs"
-	"uk.ac.bris.cs/gameoflife/util"
 )
-
-var getTurn int
-var getWorld [][]byte
-var mutex = new(sync.Mutex)
-//var cell util.Cell
-//var aliveCells []util.Cell
-//var nextWorld [][]uint8
 
 const alive = 255
 const dead = 0
 
-func getAliveNeighbour(imageHeight int, imageWidth int, x, y int, world [][]byte) int {
+func mod(x, m int) int {
+	return (x + m) % m
+}
+
+func calculateNeighbourCells(imageHeight int, imageWidth int, x, y int, world [][]byte) int {
 	neighbours := 0
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
 			if !(i == 0 && j == 0){
-				if world[modulus(y+i, imageHeight)][modulus(x+j, imageWidth)] == alive {
+				if world[mod(y+i, imageHeight)][mod(x+j, imageWidth)] == alive {
 					neighbours++
 				}
 			}
 		}
 	}
 	return neighbours
-}
-
-func modulus(i, m int) int {
-	return (i + m) % m
 }
 
 func calculateNextState(imageHeight int, imageWidth int, currentTurnWorld [][]byte) [][]byte {
@@ -47,7 +38,7 @@ func calculateNextState(imageHeight int, imageWidth int, currentTurnWorld [][]by
 
 	for y := 0; y < imageHeight; y++ {
 		for x := 0; x < imageWidth; x++ {
-			neighbours := getAliveNeighbour(imageHeight, imageWidth, x, y, currentTurnWorld)
+			neighbours := calculateNeighbourCells(imageHeight, imageWidth, x, y, currentTurnWorld)
 			if currentTurnWorld[y][x] == alive {
 				if neighbours == 2 || neighbours == 3 {
 					newWorld[y][x] = alive
@@ -66,70 +57,56 @@ func calculateNextState(imageHeight int, imageWidth int, currentTurnWorld [][]by
 	return newWorld
 }
 
-func calculateAliveCells(imageHeight int, imageWidth int, world [][]byte) []util.Cell{
-	// Prob.: had problems sending a new slice for every alive cells calculated, it was previously appending the new cell slices
-	// to the cell slices it had before.
-	var alive []util.Cell
-
-	for y := 0; y < imageHeight; y++ {
-		for x := 0; x < imageWidth; x++ {
-			if world[y][x] == 255 {
-				alive = append(alive, util.Cell{X: x, Y: y})
-			}
-		}
-	}
-	return alive
-}
-
-
 // need a method to update the world for a single iteration (similar as the ReverseString())
 // need exported method to access directly from an RPC call -> only by exported methods
 
 // similar to the update world (something like that) function to update the world for a single iteration
+// having one exported method that processes game of life?????
+//func gameOfLife(req stubs.Request, initialWorld [][]byte) [][]byte {
+
+//	world := initialWorld
+
+//	for turn := 0; turn < req.NumberOfTurns; turn++ {
+//		world = calculateNextState(0, req.HeightImage, 0, req.WidthImage, req, world)
+//	}
+//	return world
+//}
 
 type GameOfLifeOperations struct {}
-
 
 // ProcessTurns MakeGameOfLife Reverse have a for loop inside that iterates over the # of iterations specified in the Request struct
 // at the end return by the Response pointer
 func (s *GameOfLifeOperations) ProcessTurns(req stubs.Request, res *stubs.Response) (err error) {
+	//if req.ImageSize == 0 {
+	//	err = errors.New("A image size must be specified")
+	//	return
+	//}
+	//if req.NumberOfTurns == 0 {
+	//	err = errors.New("A number of turns must be specified")
+	//	return
+	//}
+
+	//if req.World == 0 {
+	//	err = errors.New("A world must be specified")
+	//	return
+	//}
+
 	// have a for loop that iterates over the # of iterations specified in the Request struct
 	// at the end returned by the Response pointer
-	//turn := getTurn
-	mutex.Lock()
-	getWorld = req.World
-	mutex.Unlock()
-	for getTurn < req.NumberOfTurns {
-		mutex.Lock()
-		getWorld = calculateNextState(req.HeightImage, req.WidthImage, getWorld)
-		getTurn++
-		mutex.Unlock()
+	for turn := 0; turn < req.NumberOfTurns; turn++ {
+		req.World = calculateNextState(req.HeightImage, req.WidthImage, req.World)
+		//gameOfLife(req, req.World)
 	}
-	//getTurn = turn
-	mutex.Lock()
-	res.World = getWorld
-	//getWorld = res.World
-	mutex.Unlock()
-	return
-}
-
-
-func (s *GameOfLifeOperations) ReportAliveCells(req stubs.CellCountRequest, res *stubs.CellCountResponse) (err error) {
-	mutex.Lock()
-	res.Turn = getTurn
-	res.AliveCells = len(calculateAliveCells(len(getWorld), len(getWorld[0]), getWorld))
-	mutex.Unlock()
-	//for getTurn < req.TotalTurns {
-	//	mutex.Lock()
-	//	res.AliveCells = len(calculateAliveCells(len(getWorld), len(getWorld[0]), getWorld))
-	//	res.Turn = getTurn
-	//	mutex.Unlock()
-	//	getTurn++
-	//}
+	res.World = req.World
 	return
 }
 
 func main(){
+	//pAddr := "8030"//flag.String("port","8030","Port to listen on")
+	//flag.Parse()
+	//rand.Seed(time.Now().UnixNano())
+	// its exported methods of GameOfLifeOperations will be called remotely
+
 	pAddr := flag.String("port","8030","Port to listen on")
 	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
@@ -137,4 +114,15 @@ func main(){
 	listener, _ := net.Listen("tcp", ":"+*pAddr)
 	defer listener.Close()
 	rpc.Accept(listener)
+
+	//g := new(GameOfLifeOperations)
+	//rpc.Register(g)
+	//rpc.HandleHTTP()
+	//listener, e := net.Listen("tcp", ":8050")
+	//if e != nil {
+	//	fmt.Println(e)
+	//	os.Exit(0)
+	//}
+	//defer listener.Close()
+	//rpc.Accept(listener)
 }
